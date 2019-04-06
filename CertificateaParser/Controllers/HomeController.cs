7d227bad83db312285bd8c410963c7a7465de837
Petr <p.sobolev@cbt.by>
2019-04-06 10:55:07 +0300

    почти сделано

--- CertificateaParser/Controllers/HomeController.cs
+++ CertificateaParser/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CertificateaParser.Models;
 using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Text;
+using Microsoft.Net.Http.Headers;
 
 namespace CertificateaParser.Controllers
 {
@@ -15,31 +18,38 @@ namespace CertificateaParser.Controllers
     {
 
         private X509Certificate2 certificate2 = new X509Certificate2();
+
         [HttpPost]
         public async Task<dynamic> UploadFile(IFormFile file)
         {
             byte[] bytes = new byte[file.Length];
             using (var reader = file.OpenReadStream())
             {
-                await reader.ReadAsync(bytes, 0, (int)file.Length);
+                await reader.ReadAsync(bytes, 0, (int) file.Length);
             }
+
             certificate2 = new X509Certificate2(bytes);
-            return View("Index",certificate2);
+            return View("Index", certificate2);
         }
+
         [HttpGet]
         public dynamic UploadFile()
         {
             return View("UploadFile");
         }
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Index(X509Certificate2 model)
         {
-            return View("Index");
+            var tempFile = CreateTmpFile();
+            System.IO.File.WriteAllText(tempFile,ExportToPEM(model));
+            return new PhysicalFileResult(tempFile,MediaTypeHeaderValue.Parse());
         }
 
         public IActionResult About()
@@ -64,7 +74,45 @@ namespace CertificateaParser.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+
+        
+
+        private static string CreateTmpFile()
+        {
+            string fileName = string.Empty;
+
+            try
+            {
+                
+                fileName = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".pem";
+
+                FileInfo fileInfo = new FileInfo(fileName);
+
+               
+                fileInfo.Attributes = FileAttributes.Temporary;
+                
+
+                Console.WriteLine("TEMP file created at: " + fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to create TEMP file or set its attributes: " + ex.Message);
+            }
+
+            return fileName;
+
+        }
+        public static string ExportToPEM(X509Certificate2 cert)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine("-----BEGIN CERTIFICATE-----");
+            builder.AppendLine(Convert.ToBase64String(cert.Export(X509ContentType.Cert), Base64FormattingOptions.InsertLineBreaks));
+            builder.AppendLine("-----END CERTIFICATE-----");
+
+            return builder.ToString();
         }
     }
 }
